c5e83045aa3386cf23024efb20413e4c1647b22a
Bradley Lyman <lyman.brad3211@gmail.com>
2024-06-27 22:57:02 -0400

    add disply for the board

--- islands_engine/lib/islands_engine/board.ex
+++ islands_engine/lib/islands_engine/board.ex
@@ -1,5 +1,41 @@
 defmodule IslandsEngine.Board do
+  alias IslandsEngine.Coordinate
+
   def new() do
     %{}
+  end
+
+  def display(board) do
+    all_hits =
+      Enum.reduce(board, MapSet.new(), fn {_name, island}, hits ->
+        MapSet.union(hits, island.hit_coordinates)
+      end)
+
+    all_coordinates =
+      Enum.reduce(board, MapSet.new(), fn {_name, island}, coords ->
+        MapSet.union(coords, island.coordinates)
+      end)
+
+    rows =
+      Enum.map(1..10, fn row ->
+        row_chars =
+          Enum.map(1..10, fn col ->
+            {:ok, coord} = Coordinate.new(row, col)
+
+            cond do
+              MapSet.member?(all_hits, coord) -> "X"
+              MapSet.member?(all_coordinates, coord) -> "O"
+              true -> "."
+            end
+          end)
+
+        "|" <> Enum.join(row_chars) <> "|\n"
+      end)
+
+    Enum.join([
+      "------------\n",
+      Enum.join(rows),
+      "------------\n"
+    ])
   end
 end
